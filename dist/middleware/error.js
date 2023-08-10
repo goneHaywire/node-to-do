7d4148bedbc762383d5328e24ab2852ff18c5d25
Emiljan Dusha <emiljanthegreat@gmail.com>
2023-08-10 14:05:26 +0200

    feat: removed dist/ from git

--- dist/middleware/error.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorMiddleware = void 0;
-const errorMiddleware = (err, req, res, next) => err.responseCallback(res).status(err.statusCode).send(err.message);
-exports.errorMiddleware = errorMiddleware;
--- dist/utils/middleware.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.applyMiddleware = void 0;
-const applyMiddleware = (middleware) => (router) => router.use(middleware);
-exports.applyMiddleware = applyMiddleware;
